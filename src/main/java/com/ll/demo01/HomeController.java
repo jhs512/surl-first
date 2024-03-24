316f21becdb19577b49314e9cfcd71e59ed105ca
jhs512 <jangka512@gmail.com>
2024-03-25 08:11:12 +0900

    work

--- src/main/java/com/ll/demo01/HomeController.java
+++ src/main/java/com/ll/demo01/HomeController.java
@@ -2,21 +2,32 @@ package com.ll.demo01;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
     @GetMapping("a")
     @ResponseBody
-    public String hello() {
-        return "Hello";
+    public String hello(
+            String age,
+            String id
+    ) {
+        return "안녕하세요. %s번 사람의 나이는 %s살 입니다.".formatted(id, age);
     }
 
     @GetMapping("b")
     @ResponseBody
-    public String hello2() {
-        System.out.println("Hello2!");
+    public String plus(
+            @RequestParam("a") String num1Str,
+            @RequestParam("b") String num2Str
+    ) {
+        int num1 = Integer.parseInt(num1Str);
+        int num2 = Integer.parseInt(num2Str);
 
-        return "안녕하세요.!!!";
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+
+        return "a + b = %d".formatted(num1 + num2);
     }
 }
